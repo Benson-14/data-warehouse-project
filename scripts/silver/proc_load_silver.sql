c7d24536e4fa98f163be4c51e812b9d97680b7d8
Benson <81464397+Benson-14@users.noreply.github.com>
2025-03-17 00:28:38 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -114,3 +114,108 @@ SELECT
 		LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day' AS DATE
 	) AS prd_end_dt -- Calc end date as one day before the next start date
 FROM bronze.crm_prd_info
+
+
+--------------------------------- crm_sales_details -------------------------------
+	
+-- Removing NULL Values 
+SELECT
+	prd_id,
+	COUNT(*)
+FROM bronze.prd_cust_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for unwanted spaces
+SELECT prd_nm FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for negative numbers or NULLs 
+SELECT prd_cost FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency 
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+-- Check for invalid DATE Orders 
+SELECT sls_order_dt FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR LEN(sls_order_dt) != 8
+
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity and Price
+-- SALES = QUANTITY * PRICE 
+-- Values must not be NULL, zero or Negative 
+SELECT DISTINCT
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price 
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+GROUP BY sls_sales, sls_quantity, sls_price
+
+SELECT
+	sls_ord_num,
+	sls_sales AS old_sls_sales,
+	sls_quantity,
+	sls_price AS old_sls_price,
+	CASE 
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+    sls_quantity,
+    CASE 
+    	WHEN sls_price IS NULL OR sls_price <= 0 
+        	THEN sls_sales / NULLIF(sls_quantity, 0)  -- Avoid division by zero
+    	ELSE sls_price  
+	END AS sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price 
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+GROUP BY sls_sales, sls_quantity, sls_price
+
+INSERT INTO silver.crm_sales_details (
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+SELECT 
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    CASE 
+		WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) != 8 THEN NULL
+		ELSE TO_DATE(sls_order_dt::TEXT, 'YYYYMMDD')
+	END AS sls_order_dt,
+    CASE 
+		WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) != 8 THEN NULL
+		ELSE TO_DATE(sls_ship_dt::TEXT, 'YYYYMMDD')
+	END AS sls_ship_dt,
+    CASE 
+		WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) != 8 THEN NULL
+		ELSE TO_DATE(sls_due_dt::TEXT, 'YYYYMMDD')
+	END AS sls_due_dt,
+	CASE 
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+    sls_quantity,
+    CASE 
+    	WHEN sls_price IS NULL OR sls_price <= 0 
+        	THEN sls_sales / NULLIF(sls_quantity, 0)  -- Avoid division by zero
+    	ELSE sls_price  
+	END AS sls_price
+FROM bronze.crm_sales_details
