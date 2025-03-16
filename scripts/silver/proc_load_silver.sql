cfffe196ed9142d8da56bfe71826d8ff9de16615
Benson <81464397+Benson-14@users.noreply.github.com>
2025-03-16 22:57:58 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -3,6 +3,8 @@ SELECT * FROM bronze.crm_prd_info
 SELECT * FROM bronze.crm_sales_details
 SELECT * FROM silver.crm_cust_info
 
+--------------------------------- crm_cust_info -------------------------------
+	
 -- Removing NULL Values 
 SELECT
 	cst_id,
@@ -56,3 +58,59 @@ FROM bronze.crm_cust_info
 WHERE flag_last = 1
 
 
+--------------------------------- crm_prd_info -------------------------------
+	
+-- Removing NULL Values 
+SELECT
+	prd_id,
+	COUNT(*)
+FROM bronze.prd_cust_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- Check for unwanted spaces
+SELECT prd_nm FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for negative numbers or NULLs 
+SELECT prd_cost FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+-- Data Standardization & Consistency 
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+-- Check for invalid DATE Orders 
+SELECT * FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+
+
+INSERT INTO silver.crm_prd_info (
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt
+)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract Category ID
+	SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key, -- Extract Product ID
+	prd_nm,
+	COALESCE(prd_cost, 0) AS prod_cost,
+	CASE 
+		WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+		WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+		WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+		WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line, --Descriptive values
+	CAST(prd_start_dt AS DATE),
+	CAST(
+		LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day' AS DATE
+	) AS prd_end_dt -- Calc end date as one day before the next start date
+FROM bronze.crm_prd_info
