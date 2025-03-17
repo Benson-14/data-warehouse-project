79b7d8e1ea635cbefa90bd65b2569576ea68d492
Benson <81464397+Benson-14@users.noreply.github.com>
2025-03-17 17:31:52 +0530

    Update and rename proc_load_silver.sql to proc_load_data_test_silver.sql

--- scripts/silver/proc_load_data_test_silver.sql
+++ scripts/silver/proc_load_data_test_silver.sql
@@ -274,3 +274,24 @@ SELECT
 		ELSE TRIM(cntry)
 	END AS cntry
 FROM bronze.erp_loc_a101
+	
+--------------------------------- erp_px_cat_g1v2 -------------------------------
+	
+-- Data Consistency & Standardization 
+SELECT DISTINCT 
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2
+
+-- Check for unwanted spaces 
+SELECT * FROM bronze.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
+
+INSERT INTO silver.erp_px_cat_g1v2 (id, cat, subcat, maintenance)
+SELECT 
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2
