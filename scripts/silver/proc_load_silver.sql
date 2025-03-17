1717781d671f2eea100897fb0690c65624ec808d
Benson <81464397+Benson-14@users.noreply.github.com>
2025-03-17 17:12:41 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -256,3 +256,21 @@ SELECT
 	END AS gen
 FROM bronze.erp_cust_az12
 
+--------------------------------- erp_loc_a101 -------------------------------
+
+-- Data Consistency & Standardization 
+SELECT DISTINCT 
+	cntry
+FROM bronze.erp_loc_a101
+ORDER BY cntry
+
+INSERT INTO silver.erp_loc_a101 (cid, cntry)
+SELECT 
+	REPLACE(cid, '-', '') AS cid,
+	CASE
+		WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+		WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'N/A'
+		ELSE TRIM(cntry)
+	END AS cntry
+FROM bronze.erp_loc_a101
